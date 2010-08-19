c2646859edfc5ef3118bc3b4a61f43905f677168
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-08-19 14:05:53 -0700

    first commit of blast_off

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'e159a3d65c5e30915de5cf0aa194db42d027f592573e17ff0814733f5bf8a792eef3fc2a5c369979c499c6f76d95e166c838d9b2048b45ea0e3e4aca947b1cf8';
