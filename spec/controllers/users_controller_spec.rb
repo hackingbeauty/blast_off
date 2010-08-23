591c0b506b4a0ce37e6036eadc5fdf6e01a9c378
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-08-23 16:51:23 -0700

    added factory girl

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,21 @@ require 'spec_helper'
 describe UsersController do
   integrate_views
   
+  describe "GET 'show'" do
+  
+    before(:each) do
+      @user = Factory(:user) 
+      # Arrange for User.find(params[:id]) to find the right user. 
+      User.stub!(:find, @user.id).and_return(@user)
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+    
+  end
+  
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
