c755aaac68ffe8968aa11d066eb6fe142441462c
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-08-23 16:42:09 -0700

    finalized password encrypted and added salts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password #virtual attribute
   
@@ -19,4 +20,47 @@ class User < ActiveRecord::Base
   validates_presence_of :password
   validates_length_of	:password, :within => 6..40
 
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email) 
+    return nil if user.nil? 
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(self.password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{self.salt}#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}#{self.password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+  
 end
+
+
+
+
+
+
+
+
+
+
+
