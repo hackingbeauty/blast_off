f5fe239b8bde33e78c1fc6be5b5105e3abbbf232
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-08-23 11:58:45 -0700

    added password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password #virtual attribute
+  
+  # Tell rails which attributes of model can be modified by users submitting requests with browser
+  # Important for preventing a mass assignment vulnerability
+  attr_accessible :name, :email, :password, :password_confirmation
   
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -7,4 +11,12 @@ class User < ActiveRecord::Base
   validates_length_of     :name, :maximum => 50
   validates_format_of     :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+  
+  # Automatically create the virtual attribute 'password_confirmation'. 
+  validates_confirmation_of :password
+
+  # Password validations.
+  validates_presence_of :password
+  validates_length_of	:password, :within => 6..40
+
 end
