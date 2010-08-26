c29f0f94b0959b5cd42baa623030199eb36b35af
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-08-26 13:25:22 -0700

    Added signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Sign Up"
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
+  
 end
